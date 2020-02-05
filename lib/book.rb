d71a83ab1bf3c0a0f1fef6d09545f50dec0ba2d6
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-05 11:07:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,7 @@
 
 
 class Book
-  
-  attr_accessor
-  :author,
-  :page_count,
-  :genre
+  attr_accessor :author, :page_count, :genre
  
   def initialize(title)
     @title = title
@@ -14,11 +10,9 @@ class Book
   def title
     @title
   end
-    
-    
-    def turn_page
-      puts "Flipping the page...wow, you read fast!"
-    end
-  
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
  
 end
